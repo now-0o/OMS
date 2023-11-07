a6e1ae5bb470025d6988224d4c31b7990595bb71
now-0o <dlwpdn0704@gmail.com>
2023-11-07 23:40:30 +0900

    feat: dotenv로 api와 엑세스 토큰 연동, request 모듈로 주문목록 조회

--- app.js
+++ app.js
@@ -5,9 +5,22 @@ app.use(express.json());
 app.use(express.static('public'));
 const indexRouter = require('./routes/index');
 
+const HttpException = require('./HttpException');
+const asyncHandler = require('./utils/asyncHandler');
+
 app.use('/', indexRouter);
 
 app.listen(port, async () => {
     console.log(`서버가 실행됩니다. http://localhost:${port}`);
   })
-  
+
+app.use((err, req, res, next) => {
+  console.log(err);
+  if(err instanceof HttpException){
+    res.status(err.status).send(err.message);
+    return;
+  }
+  res.status(500).send({
+    message : "서버에러입니다."
+  })
+})
--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,19 @@
 const path = require('path');
+const request = require('request');
+require('dotenv').config();
 
 const index = (req,res)=>{
     res.sendFile(path.join(__dirname, '..', 'views', 'index.html')); 
+
+    const options = {
+        uri: "https://api.imweb.me/v2/shop/products/0",
+        headers: {
+            'access-token': process.env.access_token
+        }
+      };
+      request(options,function(err,response,body){
+        res.json(JSON.parse(body));
+      })
 }
 
 module.exports = { index }
